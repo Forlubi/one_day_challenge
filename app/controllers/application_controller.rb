504d4cac34f2902d38f8e84e292da8a2b7378aec
wxxiao33 <infinityxwx123@gmail.com>
2020-04-28 11:35:59 -0400

    allow update without password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :avatar)}
 
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :avatar)}
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :current_password, :avatar)}
     end
 
   def after_sign_in_path_for(resource)
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,41 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
+  def update
+    new_params = params.require(:user).permit(:email, :name, :current_password, :password, :password_confirmation, :avatar)
+    change_password = true
+    if params[:user][:password].blank?
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+      new_params = params.require(:user).permit(:email, :name, :avatar)
+      change_password = false
+    end
+
+    @user = User.find(current_user.id)
+    is_valid = false
+
+    if change_password
+      is_valid = @user.update_with_password(new_params)
+    else
+      is_valid = @user.update_without_password(new_params)
+    end
+
+    if is_valid
+      set_flash_message :notice, :updated
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    else
+      render "edit"
+    end
+  end
+
   protected
 
     def after_update_path_for(resource)
       user_path(resource)
     end
+
+    
+    # def update_resource(resource, params)
+    #   resource.update_without_password(params)
+    # end
 end
