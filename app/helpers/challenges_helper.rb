056355d84d31541526bd21a9fc5a6b5cc4115c50
Anyan <anyanxie@gmail.com>
2020-05-05 15:10:05 -0400

    disable partipate/fav for archived challenges

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -2,4 +2,8 @@ module ChallengesHelper
   def challenge
     @challenge ||= Challenge.new
   end
+
+  def active_challenge? challenge
+    Date.today <= challenge.deadline - challenge.duration
+  end
 end
