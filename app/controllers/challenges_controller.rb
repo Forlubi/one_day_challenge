2b0051f58fdcb96f4858ae92e040bd763a6a1464
wxxiao33 <infinityxwx123@gmail.com>
2020-05-07 19:26:10 -0400

    add link to Cal

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -109,7 +109,7 @@ class ChallengesController < ApplicationController
   end
 
   def set_host_for_local_storage
-        ActiveStorage::Current.host = request.base_url if Rails.application.config.active_storage.service == :local
+    ActiveStorage::Current.host = request.base_url if Rails.application.config.active_storage.service == :local
   end
   
 end
